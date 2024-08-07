3b8bee9a3f22bde9737537c3736211b3cccaa56b
Vivek Gohil <ghl_vivek@hotmail.com>
2024-08-07 20:15:20 +0530

    Spring Profile Configuration

---
+++ Section 6/v2-spring-cloud-config/cards/src/main/java/com/eazybytes/cards/dto/CardsContactInfoDto.java
@@ -0,0 +1,10 @@
+package com.eazybytes.cards.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "cards")
+public record CardsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+}
