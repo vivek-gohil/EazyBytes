4db11f06e7c65f950e7e54f5aa2f84e603d391e9
Vivek Gohil <ghl_vivek@hotmail.com>
2024-12-25 17:15:00 +0530

    Service Discovery and Service Registration

---
+++ Section 9/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsContactInfoDto.java
@@ -0,0 +1,22 @@
+package com.eazybytes.accounts.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// @ConfigurationProperties(prefix = "accounts")
+// public record AccountsContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+// }
+@ConfigurationProperties(prefix = "accounts")
+@Getter
+@Setter
+public class AccountsContactInfoDto {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
---
+++ Section 9/loans/src/main/java/com/eazybytes/loans/dto/LoansContactInfoDto.java
@@ -0,0 +1,22 @@
+package com.eazybytes.loans.dto;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+// @ConfigurationProperties(prefix = "loans")
+// public record LoansContactInfoDto(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+// }
+
+@ConfigurationProperties(prefix = "loans")
+@Getter
+@Setter
+public class LoansContactInfoDto {
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
+}
