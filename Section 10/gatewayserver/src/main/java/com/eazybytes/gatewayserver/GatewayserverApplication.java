598572dea6236fd9d547e145ded9a0564e45001a
Vivek Gohil <ghl_vivek@hotmail.com>
2025-01-09 16:55:03 +0530

    Circuit Breaker implementation

--- Section 10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
+++ Section 10/gatewayserver/src/main/java/com/eazybytes/gatewayserver/GatewayserverApplication.java
@@ -1,12 +1,12 @@
 package com.eazybytes.gatewayserver;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-
-import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class GatewayserverApplication {
@@ -21,7 +21,8 @@ public class GatewayserverApplication {
 						.route(p -> p
 								.path("/eazybank/accounts/**")
 								.filters( f -> f.rewritePath("/eazybank/accounts/(?<segment>.*)","/${segment}")
-										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString()))
+										.addResponseHeader("X-Response-Time", LocalDateTime.now().toString())
+										.circuitBreaker(config -> config.setName("accountsCircuitBreaker").setFallbackUri("forward:/contactSupport")))
 								.uri("lb://ACCOUNTS"))
 					.route(p -> p
 							.path("/eazybank/loans/**")
